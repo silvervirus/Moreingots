c8941064c18b115c2c40b1ffeaa29aca14bd6124
M3dicCookie <pred102@hotmail.com>
2025-02-01 10:48:11 -0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("MoreIngots")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoreIngots")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("80d58161-b1a4-4044-b78a-3666aa10a042")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
